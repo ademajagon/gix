39064aa6b138471eb020a031a433271a6d64ed3b
Agon Ademaj <codebyagon@gmail.com>
2026-02-22 14:52:34 +0100

    feat: generate checksums sha256 on release

--- cmd/root.go
+++ cmd/root.go
@@ -26,17 +26,14 @@ var rootCmd = &cobra.Command{
 	Long:         "gix helps you write clean conventional commit messages and split staged diffs into smaller commits.",
 	Version:      version,
 	SilenceUsage: true,
-	PersistentPostRunE: func(cmd *cobra.Command, args []string) error {
-		if showUpdateNotice != nil {
-			showUpdateNotice()
-		}
-		return nil
-	},
 }
 
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
+		if showUpdateNotice != nil {
+			showUpdateNotice()
+		}
 		os.Exit(1)
 	}
 }
